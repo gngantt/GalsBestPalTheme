b6976cb63a03677cc499dcd3cba02702c692436b
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-04-03 01:17:24 -0400

    closes #10 adoption page

---
+++ page-templates/adoption.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Template Name: Template: Adoption
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+<div class="container my-5">
+    <h1>Adoption</h1>
+    <h2>Our Adoption Process</h2>
+
+    <div class="row justify-content-center">
+        <div class="col-md-11 col-lg-9">
+            <p class="text py-5">Our goal at GBP is to make rescuing a dog stress free and interactive, while 
+                also setting our adopters up for success, by thoughtfully placing the dogs we 
+                are adopting out. During the application process, our goal is to communicate 
+                and educate each person we talk to, so that when you find your perfect match 
+                and are ready to bring them home, you are prepared and ready for the next few 
+                weeks to come! We will continue to provide thoughtful advice and emotional assistance 
+                throughout the lifetime of each GBP adoption.
+            </p>
+        </div>
+    </div>    
+    <div class="row justify-content-center">
+    </div>
+    <div class="row justify-content-center">
+        <h2>Available Dogs</h2>
+    </div>
+    <div class="row justify-content-center align-items-center">
+        <div class="col-sm-12 col-md-8">
+            <h3>Check out our available dogs</h3>
+        </div>
+    </div>
+    <div class="row text-center justify-content-center align-items-center buttonImage">
+        <div class="col-sm-3 col-md-2">
+            <img class="img-fluid" 
+                src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <h2>Adoption Application</h2>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8">
+            <h3>Fill Out an Adoption Application</h3>
+        </div>
+    </div>
+    <div class="row text-center justify-content-center align-items-center buttonImage">
+        <div class="cols-sm-3 col-md-2">
+            <img class="img-fluid" 
+                src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <h2>Helpful Resources</h2>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8">
+            <h3>Setting Adopters Up for Success</h3>
+        </div>
+    </div>
+    <div class="row text-center justify-content-center align-items-center buttonImage">
+        <div class="col-sm-3 col-md-2">
+            <img class="img-fluid" 
+                src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
