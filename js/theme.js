106f2aa38f86adf35b07799857db06e506ddd223
Daniel Vigil <daniel.julian.vigil@gmail.com>
2022-04-11 00:08:45 -0400

    #35 adding truncation of larger text on home screen

--- js/theme.js
+++ js/theme.js
@@ -9296,6 +9296,20 @@
 	  }
 	})();
 
+	// Add your JS customizations here
+	let readMore = document.getElementsByClassName('readMore')[0];
+	readMore.addEventListener('click', () => {
+	  let text = document.getElementsByClassName('text')[0];
+
+	  if (text.style.display === 'block') {
+	    text.style.display = '-webkit-box';
+	    readMore.innerHTML = 'Read more';
+	  } else {
+	    text.style.display = 'block';
+	    readMore.innerHTML = 'Read less';
+	  }
+	});
+
 	exports.Alert = alert;
 	exports.Button = button;
 	exports.Carousel = carousel;
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -1,1 +1,14 @@
 // Add your JS customizations here
+let readMore = document.getElementsByClassName('readMore')[0];
+
+readMore.addEventListener('click', () => {
+    let text = document.getElementsByClassName('text')[0];
+
+    if (text.style.display === 'block') {
+        text.style.display = '-webkit-box';
+        readMore.innerHTML = 'Read more'
+    } else {
+        text.style.display = 'block';
+        readMore.innerHTML = 'Read less'
+    }
+});
