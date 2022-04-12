15e7e8f332fc98f13cc3b83ed153f358de5eea63
Hanna <hannamlittlefield@gmail.com>
2022-04-12 16:14:57 -0400

    editing Volunteer page to look like the other templates

--- page-templates/volunteer.php
+++ page-templates/volunteer.php
@@ -2,8 +2,7 @@
 /**
  * Template Name: Template: Volunteer
  *
- * Template for displaying a page just with the header and footer area and a "naked" content area in between.
- * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ * Foster Page Template
  *
  * @package Understrap
  */
@@ -17,58 +16,51 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="container my-5">
     <h1>Volunteer</h1>
-    <h3>Become part of the mission</h3>
+    <h2>Be part of the mission!</h2>
 
     <div class="row justify-content-center">
         <div class="col-md-11 col-lg-9">
-            <p class="text pt-0 ">We are all volunteers! The GBP RESCUE MISSION was formed on that very idea. This rescue has so many moving parts and the more the merrier is the way we look at it! We love our volunteers and believe every person has a place in rescue! We believe in communication and mutual respect throughout the organization. Everyone is equally as important here from the dog walkers, to the Directors. We want to hear your ideas and utilize everyone's personal strengths! Join us! 
+            <p class="text py-5">We are all volunteers! The GBP RESCUE MISSION was formed on that very idea. This rescue has so many moving parts and the more the merrier is the way we look at it! We love our volunteers and believe every person has a place in rescue! We believe in communication and mutual respect throughout the organization. Everyone is equally as important here from the dog walkers, to the Directors. We want to hear your ideas and utilize everyone’s personal strengths! Join us! 
+
             </p>
         </div>
-    </div>
-        </div>
     </div>    
+    <div class="row justify-content-center">
+    </div>
+    <div class="row justify-content-center">
+        <h2>About the Program</h2>
+    </div>
     <div class="row justify-content-center align-items-center">
         <div class="col-sm-12 col-md-8">
-            
+            <h3>Our Volunteer Program</h3>
         </div>
     </div>
-    <div class="row">
-        <div class="col">
-            <h2>About the Program</h2>
-            <h3>Our volunteer program</h3>
-        </div>
-        <div class="col">
-        <img class="img-fluid" 
-                src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
-                alt="Tennis ball button"
-                height="80"
-                width="80"/>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <h2>Volunteer Application</h2>
-            <h3>Join the mission</h3>
-        </div>
-        <div class="col">
+    <div class="row text-center justify-content-center align-items-center buttonImage">
+        <div class="col-sm-3 col-md-2">
             <img class="img-fluid" 
                 src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
-                alt="amazon logo"
-                height="80"
-                width="80"/>
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
         </div>
     </div>
     <div class="row justify-content-center">
-			<div class="col-9">
+        <h2>Volunteer Application</h2>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-sm-12 col-md-8">
+            <h3>Join the mission</h3>
+        </div>
+    </div>
+    <div class="row text-center justify-content-center align-items-center buttonImage">
+        <div class="cols-sm-3 col-md-2">
             <img class="img-fluid" 
-                src="<?php echo get_template_directory_uri(); ?>/images/lineofdogs.svg" 
-                alt="line of dogs running up to a man"
-                height="300"
-                width="300"/>
-				<h2>We are an all volunteer run Massachusetts 501 C 3 Nonprofit Org!</h2>
-			</div>
-		</div>
-
+                src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
+        </div>
+    </div>
 </div>
 
 <?php
