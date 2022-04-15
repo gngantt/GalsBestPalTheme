01fd6be2a74e85a89d95677ba5d66f296ccce4ee
Hanna <hannamlittlefield@gmail.com>
2022-04-15 16:54:35 -0400

    adding arrow class to image removes tennis ball as well

--- page-templates/volunteer.php
+++ page-templates/volunteer.php
@@ -68,7 +68,7 @@ $container = get_theme_mod( 'understrap_container_type' );
                 height="100"
                 width="100"/>
         </div>
-        <div class="cols-sm-3 col-md-2">
+        <div class="col-sm-3 col-md-2">
             <img class="img-fluid" 
                 src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
                 alt="Gals Best Pal logo"
