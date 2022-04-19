8a543d3e19a0357cd10a96bba5b7e5df33fe5802
Hanna <hannamlittlefield@gmail.com>
2022-04-19 09:22:18 -0400

    turning application image to button and adding pop up class

--- page-templates/adoption.php
+++ page-templates/adoption.php
@@ -35,12 +35,11 @@ $container = get_theme_mod( 'understrap_container_type' );
         </div>
     <div class="row justify-content-center">
         <div class="col-sm-3 col-md-2 ">
-            <img class="img-fluid pb-5" 
-                src="<?php echo get_template_directory_uri(); ?>/images/pgbreakdog.svg" 
-                alt="Gals Best Pal logo"
-                height="150"
-                width="150"/>
-        
+                <img class="img-fluid pb-5" 
+                    src="<?php echo get_template_directory_uri(); ?>/images/pgbreakdog.svg" 
+                    alt="Gals Best Pal logo"
+                    height="150"
+                    width="150"/>
     </div>    
     <div class="row justify-content-center">
     </div>
@@ -72,11 +71,13 @@ $container = get_theme_mod( 'understrap_container_type' );
     </div>
     <div class="row text-center justify-content-center align-items-center buttonImage">
         <div class="cols-sm-3 col-md-2">
+        <a role="button" class="photo-btn sg-popup-id-45" href="#">
             <img class="img-fluid" 
                 src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
                 alt="Gals Best Pal logo"
                 height="100"
                 width="100"/>
+        </a>
         </div>
     </div>
     <div class="row justify-content-center">
