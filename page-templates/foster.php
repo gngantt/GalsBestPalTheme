b923c71d40cd6cd598b1edc5fd918f8c84f47c93
Hanna <hannamlittlefield@gmail.com>
2022-05-23 12:15:05 -0400

    adding arrows on foster page

--- page-templates/foster.php
+++ page-templates/foster.php
@@ -48,6 +48,13 @@ $container = get_theme_mod( 'understrap_container_type' );
         </div>
     </div>
     <div class="row text-center justify-content-center align-items-center buttonImage">
+    <div class="col-sm-3 col-md-2 arrow">
+            <img class="img-fluid" 
+                src="<?php echo get_template_directory_uri(); ?>/images/arrowcrop.gif" 
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
+        </div>
         <div class="col-sm-3 col-md-2">
             <img class="img-fluid" 
                 src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
@@ -65,6 +72,13 @@ $container = get_theme_mod( 'understrap_container_type' );
         </div>
     </div>
     <div class="row text-center justify-content-center align-items-center buttonImage">
+        <div class="col-sm-3 col-md-2 arrow">
+            <img class="img-fluid" 
+                src="<?php echo get_template_directory_uri(); ?>/images/arrowcrop.gif" 
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
+        </div>
         <div class="cols-sm-3 col-md-2">
             <img class="img-fluid" 
                 src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
@@ -82,6 +96,13 @@ $container = get_theme_mod( 'understrap_container_type' );
         </div>
     </div>
     <div class="row text-center justify-content-center align-items-center buttonImage">
+        <div class="col-sm-3 col-md-2 arrow">
+            <img class="img-fluid" 
+                src="<?php echo get_template_directory_uri(); ?>/images/arrowcrop.gif" 
+                alt="Gals Best Pal logo"
+                height="100"
+                width="100"/>
+        </div>
         <div class="col-sm-3 col-md-2">
             <img class="img-fluid" 
                 src="<?php echo get_template_directory_uri(); ?>/images/tennisball.svg" 
