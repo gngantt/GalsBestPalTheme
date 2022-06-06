a9664547535ece75639b439b10df60ebca24cd09
Hanna <hannamlittlefield@gmail.com>
2022-06-06 10:06:51 -0400

    adding looking for options link

--- page-templates/contact.php
+++ page-templates/contact.php
@@ -64,11 +64,13 @@ $container = get_theme_mod( 'understrap_container_type' );
     </div>
     <div class="row text-center justify-content-center align-items-center buttonImage">
         <div class="col-sm-3 col-md-2">
-            <img class="img-fluid" 
-                src="<?php echo get_template_directory_uri(); ?>/images/envelope.svg" 
-                alt="Gals Best Pal logo"
-                height="200"
-                width="200"/>
+            <a href="https://www.canva.com/design/DAFC1Q20WT0/1cIV0Ns1ziuMG9tXCluftQ/view?utm_content=DAFC1Q20WT0&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank">
+                <img class="img-fluid" 
+                    src="<?php echo get_template_directory_uri(); ?>/images/envelope.svg" 
+                    alt="Gals Best Pal logo"
+                    height="200"
+                    width="200"/>
+            </a>
         </div>
     </div>
     <div class="row justify-content-center">
