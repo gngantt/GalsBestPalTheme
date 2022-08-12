60b4f0796b68be27ea92595c8998b86eaa416b48
Gabby Gantt <gngantt@gmail.com>
2022-08-12 16:16:28 -0400

    Update amazon-shop.php

--- page-templates/amazon-shop.php
+++ page-templates/amazon-shop.php
@@ -1,31 +1,34 @@
 <?php
 /**
- * Template Name: Amazon Shop
+ * Template Name: Template: Donate
  *
- * Template for displaying a blank page.
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
  *
  * @package Understrap
  */
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
 ?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<?php wp_head(); ?>
-</head>
-<body>
-	<?php
-	while ( have_posts() ) {
-		the_post();
-		get_template_part( 'loop-templates/content', 'blank' );
-	}
-	wp_footer();
-	?>
-</body>
-</html>
+
+<div class="container my-5">
+	
+	<h1>Amazon Affiliate Shop</h1>
+	
+	<p>
+		Shop here for all your new adoption needs and help fund GBP in the process. 
+	</p>
+
+    
+	<a href="https://www.amazon.com/PetSafe-Martingale-Collar-Buckle-Raspberry/dp/B00CZ7HZD6?crid=2RRGXKW9DH9PB&keywords=martingale%2Bcollar%2Bfor%2Bdogs&qid=1660335045&sprefix=martinga%2Caps%2C86&sr=8-1-spons&th=1&linkCode=li2&tag=galsbestpal03-20&linkId=6118a125a8c6025667bdaf5235421715&language=en_US&ref_=as_li_ss_il" target="_blank"><img border="0" src="//ws-na.amazon-adsystem.com/widgets/q?_encoding=UTF8&ASIN=B00CZ7HZD6&Format=_SL160_&ID=AsinImage&MarketPlace=US&ServiceVersion=20070822&WS=1&tag=galsbestpal03-20&language=en_US" ></a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=galsbestpal03-20&language=en_US&l=li2&o=1&a=B00CZ7HZD6" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />
+	
+	
+	
+</div>
+
+<?php
+get_footer();
