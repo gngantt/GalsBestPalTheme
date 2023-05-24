023b5a48307bc939ef6ebe1a446722bdaae13d30
Gabby Gantt <gngantt@gmail.com>
2023-05-23 21:59:11 -0400

    update

--- page-templates/shop.php
+++ page-templates/shop.php
@@ -23,27 +23,27 @@ $container = get_theme_mod( 'understrap_container_type' );
 	
 
 	<br><br>
+	<h2>Shop Amazon Products</h2>
+	<p class="text">Shop our Amazon Affiliate store for suggested products for your new PAL and a portion of the sales will help GBP!</p>
+	
 	<div class="row justify-content-center"><br>
 			<h2><a href="https://galsbestpal.com/wp-content/uploads/2023/01/AMAZON-SHOP.pdf" target="_blank" style="font-size:24px; padding: 10px; border: 3px solid black;">Amazon Affiliate Shop</a></h2><br><br><br>
-        </div>
+        </div><br>
 	
    
-	<br><div class="row justify-content-center">
-        <h2>Other Ways to Support us Through Amazon</h2>
-    </div>
-  
-	<div class="row ustify-content-center">
-			<a href="https://galsbestpal.com/wp-content/uploads/2022/09/AmazonAngles.pdf">
-			<img class="img-fluid" 
-				style="padding: 10px"
-                src="<?php echo get_template_directory_uri(); ?>/images/Amazon.jpeg" 
-                alt="Gals Best Pal logo"
-                height="175"
-                width="175"/>
-						</a><br><br>
-			</div>
+	<h2>Bonfire Store</h2>
+	<p class="text">Click the link below to shop GBP Swag! Each item ships directly to you!</p>
 	
-	<a target="_blank" href="https://shareasale.com/r.cfm?b=1626944&amp;u=3408493&amp;m=66364&amp;urllink=&amp;afftrack="><img src="https://static.shareasale.com/image/66364/Embark_Affiliate_Ads_FINAL_392x72.jpg" border="0" /></a>
+	<div class="row justify-content-center">
+					<a target="_blank" href="https://www.bonfire.com/store/gals-best-pal/">
+			<img class="img-fluid" 
+                src="<?php echo get_template_directory_uri(); ?>/images/bonfire.jpeg" 
+                alt="Gals Best Pal logo"
+                height="250"
+                width="250"/>
+						</a>
+       	 			</div>
+				
 	
 </div>
 
