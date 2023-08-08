6d3616f6a5ba0bfad269c832d8665550259138c1
Gabby Gantt <gngantt@gmail.com>
2023-08-08 19:49:57 -0400

    events

--- page-templates/Events.php
+++ page-templates/Events.php
@@ -24,17 +24,35 @@ $container = get_theme_mod( 'understrap_container_type' );
 				
 				<img class="img-fluid" 
 				style="padding: 10px"
-                src="<?php echo get_template_directory_uri(); ?>/images/july-drink.jpg" 
+                src="<?php echo get_template_directory_uri(); ?>/images/drink-august.jpg" 
                 alt="Gals Best Pal logo"
                 height="800"
                 width="800"/>
 				
 
-			<a target="_blank" href="https://www.facebook.com/galsbestpal">
-			
+			<a target="_blank" href="https://www.facebook.com/events/1999702820398027?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22your_upcoming_events_unit%22%2C%22surface%22%3A%22bookmark%22%7D]%2C%22ref_notif_type%22%3Anull%7D">
 			<img class="img-fluid" 
 				style="padding: 10px"
-                src="<?php echo get_template_directory_uri(); ?>/images/8-12-natural_dog.jpg" 
+                src="<?php echo get_template_directory_uri(); ?>/images/8-12_NaturalDog.jpg" 
+                alt="Gals Best Pal logo"
+                height="800"
+                width="800"/>
+				</a>
+				
+				
+			<a target="_blank" href="https://www.facebook.com/galsbestpal">
+			<img class="img-fluid" 
+				style="padding: 10px"
+                src="<?php echo get_template_directory_uri(); ?>/images/9_16_TractorSupply.jpg" 
+                alt="Gals Best Pal logo"
+                height="800"
+                width="800"/>
+				</a>
+				
+			<a target="_blank" href="https://www.facebook.com/galsbestpal">
+			<img class="img-fluid" 
+				style="padding: 10px"
+                src="<?php echo get_template_directory_uri(); ?>/images/9_21_YappyHour.jpg" 
                 alt="Gals Best Pal logo"
                 height="800"
                 width="800"/>
